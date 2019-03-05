0e3701b1e346b3112cc352ab40ef0cc75c9375a1
John Oncher <johnoncher@gmail.com>
2019-03-05 11:55:56 -0500

    create books and save to database

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
-class BooksController << ApplicationController
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
 
   def create
     @book = Book.find_or_create_by(book_params)
     render json: @book
   end
-end
 
-private
 
-def book_params
-  params.permit(:title, :author, :publisher, :description, :isbn_10, :page_count, :average_review, :thumbnail, :info_link, :retail_price)
+  private
+
+  def book_params
+    params.permit(:title, :author, :publisher, :description, :isbn_10, :page_count, :average_review, :thumbnail, :info_link, :retail_price)
+  end
+
 end
