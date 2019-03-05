d2d8d1ca135fa22497f759c2304e42fc52345b41
rchow1944 <rchow2@buffalo.edu>
2019-03-05 15:42:48 -0500

    render book list added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  def get_my_books
+    @user = User.find(params[:id])
+    render json: @user.books
+  end
+
   def delete_book
     @user_book = UserBook.find_by(user_id: params[:user_id], book_id: params[:id])
     @user_book.delete
